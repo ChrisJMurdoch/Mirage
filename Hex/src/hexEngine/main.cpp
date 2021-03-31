60a317e3663272a35cd9b75382f39c2d8058b13d
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2021-03-31 01:22:46 +0100

    Started implementing array buffer

--- Hex/src/hexEngine/main.cpp
+++ Hex/src/hexEngine/main.cpp
@@ -3,6 +3,7 @@
 
 #include "render/display.hpp"
 #include "model/instance.hpp"
+#include "arrayBuffer/arrayBuffer.hpp"
 #include "hexEngine/hexCell.hpp"
 
 #include <iostream>
@@ -12,6 +13,17 @@ int main()
 {
     try
     {
+        PNC buffer(2);
+
+        buffer.position(0)->xyz(3, 3, 3);
+        buffer.normal(0)->xyz(0, 1, 0);
+        buffer.colour(0)->xyz(250, 250, 250);
+        buffer.position(1)->xyz(4, 4, 4);
+        buffer.normal(1)->xyz(0.5f, 0, 0.5f);
+        buffer.colour(1)->xyz(255, 255, 255);
+
+        buffer.bufferData(0);
+
         // Create display and opengl context
         Display display(1000, 600, "WorldEngine");
 
