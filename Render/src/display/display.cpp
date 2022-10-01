30ba181a5493f2c4761ce189ac0731a1d8134a37
Chris Murdoch <christopherjmurdoch@gmail.com>
2022-10-01 23:51:05 +0100

    Changed background colour

--- Render/src/display/display.cpp
+++ Render/src/display/display.cpp
@@ -62,7 +62,7 @@ void Display::render()
         glfwSetWindowShouldClose(window, true);
     
     // Clear viewport
-    glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+    glClearColor(0.15f, 0.15f, 0.15f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Render each model
